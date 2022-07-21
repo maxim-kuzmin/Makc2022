73cd723d7568b4ff07f69c7b5709c54c8c9c527d
maxim-kuzmin <maxim.kuzmin@inbox.ru>
2022-07-21 22:02:11 +0300

    -

--- server/makc2022--dotnet/Makc2022.Layer5.Sql.Server/Setting/SettingModule.cs
+++ server/makc2022--dotnet/Makc2022.Layer5.Sql.Server/Setting/SettingModule.cs
@@ -1,7 +1,16 @@
 ﻿// Copyright (c) 2022 Maxim Kuzmin. All rights reserved. Licensed under the MIT License.
 
 using Makc2022.Layer1.Common;
+using Makc2022.Layer2.Sql.Common;
+using Makc2022.Layer5.Sql.Server.Pages.DummyMain.Item;
+using Makc2022.Layer5.Sql.Server.Pages.DummyMain.List;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+using IClientProviderForSqlServer = Makc2022.Layer2.Sql.Clients.SqlServer.ClientProvider;
+using IDummyMainDomainItemGetQueryHandler = Makc2022.Layer4.Sql.Domains.DummyMain.Queries.Item.Get.IDomainItemGetQueryHandler;
+using IDummyMainDomainListGetQueryHandler = Makc2022.Layer4.Sql.Domains.DummyMain.Queries.List.Get.IDomainListGetQueryHandler;
+using IDummyMainDomainService = Makc2022.Layer4.Sql.Domains.DummyMain.IDomainService;
 
 namespace Makc2022.Layer5.Sql.Server.Setting
 {
@@ -18,6 +27,18 @@ namespace Makc2022.Layer5.Sql.Server.Setting
             services.AddSingleton(new CommonEnvironment());
 
             services.AddLocalization(x => x.ConfigureLocalization());
+
+            services.AddSingleton<ICommonProvider>(x => x.GetRequiredService<IClientProviderForSqlServer>());
+
+            services.AddScoped<IDummyMainItemPageService>(x => new DummyMainItemPageService(
+                x.GetRequiredService<IDummyMainDomainItemGetQueryHandler>(),
+                x.GetRequiredService<IDummyMainDomainService>()
+                ));
+
+            services.AddScoped<IDummyMainListPageService>(x => new DummyMainListPageService(
+                x.GetRequiredService<IDummyMainDomainListGetQueryHandler>(),
+                x.GetRequiredService<IDummyMainDomainService>()
+                ));
         }
 
         /// <inheritdoc/>
@@ -25,10 +46,31 @@ namespace Makc2022.Layer5.Sql.Server.Setting
         {
             return new[]
             {
-                typeof(CommonEnvironment)
+                typeof(CommonEnvironment),
+                typeof(ICommonProvider),
+                typeof(IDummyMainItemPageService),
+                typeof(IDummyMainListPageService),
+                typeof(ILogger),
+                typeof(IStringLocalizer),
             };
         }
 
         #endregion Public methods
+
+        #region Protected methods
+
+        /// <inheritdoc/>
+        protected override IEnumerable<Type> GetImports()
+        {
+            return new[]
+            {
+                typeof(IClientProviderForSqlServer),
+                typeof(IDummyMainItemPageService),
+                typeof(IDummyMainListPageService),
+                typeof(IDummyMainDomainService)
+            };
+        }
+
+        #endregion Protected method
     }
 }
