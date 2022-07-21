196255e8fcb13868c8ca441e785107868554e49e
maxim-kuzmin <maxim.kuzmin@inbox.ru>
2022-07-21 22:34:48 +0300

    -

---
+++ server/makc2022--dotnet/Makc2022.Layer5.Sql.GrpcClient/Setting/SettingExtension.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) 2022 Maxim Kuzmin. All rights reserved. Licensed under the MIT License.
+
+using Makc2022.Layer1.Common;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Layer5SqlModuleForGrpcClient = Makc2022.Layer5.Sql.GrpcClient.Setting.SettingModule;
+
+namespace Makc2022.Layer5.Sql.GrpcClient.Setting
+{
+    /// <summary>
+    /// Расширение настройки.
+    /// </summary>
+    public static class SettingExtension
+    {
+        #region Public methods
+
+        /// <summary>
+        /// Добавить сервисы приложения.
+        /// </summary>
+        /// <param name="services">Сервисы.</param>
+        /// <param name="configuration">Конфигурация.</param>
+        public static void AddAppServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            const string root = "Makc2022";
+
+            services.AddAppModules(new CommonModule[]
+            {
+                new Layer5SqlModuleForGrpcClient(configuration.GetRequiredSection($"{root}:Layer5:Sql:GrpcClient"))
+            });
+        }
+
+        #endregion Public methods
+    }
+}
---
+++ server/makc2022--dotnet/Makc2022.Layer5.Sql.GrpcClient/Setting/SettingModule.cs
@@ -0,0 +1,89 @@
+﻿// Copyright (c) 2022 Maxim Kuzmin. All rights reserved. Licensed under the MIT License.
+
+using Makc2022.Layer1.Common;
+using Makc2022.Layer5.Sql.GrpcServer.Protos.Pages.DummyMain.Item;
+using Makc2022.Layer5.Sql.GrpcServer.Protos.Pages.DummyMain.List;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Makc2022.Layer5.Sql.GrpcClient.Setting
+{
+    /// <summary>
+    /// Модуль настройки.
+    /// </summary>
+    public class SettingModule : CommonModule
+    {
+        #region Properties
+
+        private IConfigurationSection ConfigurationSection { get; set; }
+
+        #endregion Properties
+
+        #region Constructors
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="configurationSection">Раздел конфигурации.</param>
+        public SettingModule(IConfigurationSection configurationSection)
+        {
+            ConfigurationSection = configurationSection;
+        }
+
+        #endregion Constructors
+
+        #region Public methods
+
+        /// <inheritdoc/>
+        public sealed override void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(new CommonEnvironment());
+
+            services.AddLocalization(x => x.ConfigureLocalization());
+
+            services.Configure<SettingOptions>(ConfigurationSection);
+
+            services.AddGrpcClient<DummyMainItemPage.DummyMainItemPageClient>((serviceProvider, options) =>
+            {
+                options.Address = new Uri(serviceProvider.GetRequiredService<IOptions<SettingOptions>>().Value.ServerUrl!);
+            });
+
+            services.AddGrpcClient<DummyMainListPage.DummyMainListPageClient>((serviceProvider, options) =>
+            {
+                options.Address = new Uri(serviceProvider.GetRequiredService<IOptions<SettingOptions>>().Value.ServerUrl!);
+            });
+        }
+
+        /// <inheritdoc/>
+        public sealed override IEnumerable<Type> GetExports()
+        {
+            return new[]
+            {
+                typeof(CommonEnvironment),                
+                typeof(DummyMainItemPage.DummyMainItemPageClient),
+                typeof(DummyMainListPage.DummyMainListPageClient),
+                typeof(IOptions<SettingOptions>),
+                typeof(ILogger),
+                typeof(IStringLocalizer),
+            };
+        }
+
+        #endregion Public methods
+
+        #region Protected methods
+
+        /// <inheritdoc/>
+        protected override IEnumerable<Type> GetImports()
+        {
+            return new[]
+            {
+                typeof(IOptions<SettingOptions>),
+            };
+        }
+
+        #endregion Protected method
+    }
+}
